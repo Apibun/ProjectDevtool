2d2582de0e18a08669c874bbafdc4a3a1141baaa
Apibun <63070184@kmitl.ac.th>
2023-04-13 00:53:04 +0700

    Update agency get data and insert report from modal

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require('multer');
 const app = express();
 const port = 3000;
 
@@ -8,8 +9,13 @@ app.use(express.json());
 
 const productRouter = require("./router/product");
 const newsRouter = require("./router/news");
+// const projectRouter = require("./router/projects");
+const agencyRouter = require("./router/agency");
+
 app.use(productRouter.router);
 app.use(newsRouter.router);
+// app.use(projectRouter.router);
+app.use(agencyRouter.router);
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!");
--- backend/router/agency.js
+++ backend/router/agency.js
@@ -1,8 +1,49 @@
-
-const express = require('express')
+const express = require("express");
 router = express.Router();
+const multer = require("multer");
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb://127.0.0.1:27017";
 
+// Set up Multer for handling file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./static/uploads"); // Specify the upload directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Generate a unique file name
+  },
+});
+const upload = multer({ storage: storage });
+
+router.get("/agency", async (req, res) => {
+  const client = new MongoClient(uri);
+  await client.connect();
+  const getAgency = await client
+    .db("DevTool")
+    .collection("agency")
+    .find({})
+    .toArray();
+  await client.close();
+  res.status(200).send(getAgency);
+});
+
+router.post("/agency", upload.single("image"), async (req, res) => {
+  console.log(req.file);
+  console.log(req.file.path);
+  const agency = req.body;
+  console.log(agency);
+  const client = new MongoClient(uri);
+  await client.connect();
+  await client
+    .db("DevTool")
+    .collection("report")
+    .insertOne({
+      image: req.file.path.substring(6),
+      heading: agency.heading,
+      inform: agency.inform,
+    });
+  await client.close();
+  res.status(200).json({ message: "Upload Image Successfully" });
+});
 
 exports.router = router;
